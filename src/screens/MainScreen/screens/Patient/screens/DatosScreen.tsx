60aa42f1b918c76c376ebb322df053aaba293845
brandonhsz <brandonhszzz@gmail.com>
2023-11-16 04:08:03 -0600

    feat: datos

--- src/screens/MainScreen/screens/Patient/screens/DatosScreen.tsx
+++ src/screens/MainScreen/screens/Patient/screens/DatosScreen.tsx
@@ -1,15 +1,50 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { usePatientStore } from "../../../../../state/userState";
+import { useTheme } from "react-native-paper";
 
 export default function DatosScreen() {
   const patient = usePatientStore((state) => state.patient);
-  console.log(patient);
+  const theme = useTheme();
   return (
-    <View>
-      <Text>Nombre: {patient.name}</Text>
-      <Text>Edad: {patient.age}</Text>
-      <Text>Peso: {patient.weight}</Text>
+    <View
+      style={{
+        marginHorizontal: 20,
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          justifyContent: "center",
+          flex: 1,
+        }}
+      >
+        <Text style={{ fontSize: 30, alignSelf: "center", fontWeight: "bold" }}>
+          Datos del Paciente:
+        </Text>
+
+        <View
+          style={{
+            backgroundColor: theme.colors.primary,
+            padding: 20,
+            borderRadius: 20,
+            elevation: 5,
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 25 }}>
+            Nombre: {patient.name}
+          </Text>
+          <Text style={{ color: "white", fontSize: 25 }}>
+            Edad: {patient.age}
+          </Text>
+          <Text style={{ color: "white", fontSize: 25 }}>
+            Peso: {patient.weight}
+          </Text>
+          <Text style={{ color: "white", fontSize: 25 }}>
+            Telefono: {patient.phone}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
